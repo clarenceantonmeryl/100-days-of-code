1f3661f14b859890e2e14c8a8db820a6f9128ca8
clarence <clarence.antonmeryl@gmail.com>
2021-12-14 12:53:30 +1100

    Add Day 8, Caesar shift - update

--- day_08_caesar_shift.py
+++ day_08_caesar_shift.py
@@ -2,33 +2,30 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 
 def character_shifter(shift, character):
-    index = alphabet.index(character) + shift
+    index = alphabet.index(character) + (shift % 26)
     if index > 25:
         index -= 26
     return alphabet[index]
 
 
 def caesar_shift(direction, text, shift):
-    if direction == "decode":
+    if direction == "d":
         shift = shift * -1
-
     output = ""
-
     for char in text:
         if char.isalpha():
             output += character_shifter(shift=shift, character=char)
         else:
             output += char
-
     print(output)
 
 
 while True:
-    direction_input = input("Type 'encode' to encrypt, type 'decode' to decrypt, type 'exit' to exit :\n")
+    direction_input = input("Type 'e' to encrypt, type 'd' to decrypt, type 'x' to exit :\n")
     text_input = input("Type your message:\n").lower()
     shift_input = int(input("Type the shift number:\n"))
 
-    if direction_input == "exit":
+    if direction_input == "x":
         break
     else:
         caesar_shift(direction=direction_input, text=text_input, shift=shift_input)
